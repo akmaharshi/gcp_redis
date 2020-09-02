8479acab42c7551527535ddbfb18ce9186b5f177
Anil Kumar Maharshi Pemmaraju <anilmaharshi.p@gmail.com>
2020-09-02 10:12:37 +0530

    cron added

--- myredis.sh
+++ myredis.sh
@@ -23,7 +23,13 @@ REDIS_ROLE=`${REDIS_CLI} info | grep ^role: | cut -f 2 -d ':'`
 if [[ "${REDIS_ROLE}" == "master" ]]; then
     SLAVE_NAME=`${REDIS_CLI} info | grep slave0 | cut -f 2 -d '=' |  cut -f 1 -d ',' `
     ${REDIS_CLI} slaveof ${SLAVE_NAME} ${REDIS_PORT}
+    crontab -l | grep -v backup-redis > jobs.txt
+    echo "11 1,5,9,13,17,21 * * * /usr/local/runscope/ops-tools/bin/backup-redis 6379 >/dev/null 2>&1" >> jobs.txt
+    cron jobs.txt
 else 
     ${REDIS_CLI} slaveof no one
     printf "Converted Slave host to master" >> $logFile
+    crontab -l | grep -v backup-redis > jobs.txt
+    echo "#11 1,5,9,13,17,21 * * * /usr/local/runscope/ops-tools/bin/backup-redis 6379 >/dev/null 2>&1" >> jobs.txt
+    cron jobs.txt
 fi
